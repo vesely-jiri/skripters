63a3fcf55cb134968d852b4c6f0e269939ff7573
Jiří Veselý <vesely.jiri1998@gmail.com>
2025-07-03 14:32:42 +0200

    refactor: domain package change

--- services/skripters-api-gateway/src/main/java/sk/skripters/apigateway/SkriptersApiGateway.java
+++ services/skripters-api-gateway/src/main/java/sk/skripters/apigateway/SkriptersApiGateway.java
@@ -1,4 +1,4 @@
-package sk.skripters;
+package sk.skripters.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- services/skripters-auth/src/main/java/sk/skripters/auth/SkriptersAuth.java
+++ services/skripters-auth/src/main/java/sk/skripters/auth/SkriptersAuth.java
@@ -1,4 +1,4 @@
-package sk.skripters;
+package sk.skripters.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
