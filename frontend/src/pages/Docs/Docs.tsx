6546077ce605dd0949f9e2bbc7ebcdcfca9a10cf
Jiří Veselý <vesely.jiri1998@gmail.com>
2025-07-18 10:28:50 +0200

    fix: implement new Logo structure

--- frontend/src/pages/Docs/Docs.tsx
+++ frontend/src/pages/Docs/Docs.tsx
@@ -1,4 +1,5 @@
 import TypeFilterMenu from './components/TypeFilter/TypeFilterMenu'
+import Logo from '../../components/logo/Logo'
 
 import styles from './Docs.module.css'
 
@@ -9,8 +10,8 @@ const Docs = () => {
                 <TypeFilterMenu />
             </div>
 
-            <div>
-                <h1>Docs</h1>
+            <div className={styles.logo}>
+                <Logo subtitle='Docs'/>
             </div>
         </>
     )
