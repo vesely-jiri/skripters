f3a3bdd90ec68d08e48491d6bcb2b397d88576ef
Jiří Veselý <vesely.jiri1998@gmail.com>
2025-07-18 09:09:48 +0200

    feat: add base routing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import './index.css'
-import App from './App.tsx'
+
+import AppLayout from './AppLayout'
+import Home from './pages/Home/Home'
+import Docs from './pages/Docs/Docs'
+import Forum from './pages/Forum/Forum'
+//import Profile from './pages/Profile/Profile'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [
+      { path: '', element: <Home /> },
+      { path: 'docs', element: <Docs /> },
+      { path: 'forum', element: <Forum /> },
+//      { path: 'profile', element: <Profile /> },
+    ],
+  },
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
