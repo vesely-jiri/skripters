f016b9d4bd7b5ebe608fb8b91d8fc12ebe901a69
Jiří Veselý <vesely.jiri1998@gmail.com>
2025-07-19 12:54:26 +0200

    chore: Rename docs main file

---
+++ services/skripters-docs/src/main/java/sk/skripters/docs/SkriptersDocs.java
@@ -0,0 +1,13 @@
+package sk.skripters.docs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkriptersDocs {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        SpringApplication.run(SkriptersDocs.class, args);
+    }
+}
