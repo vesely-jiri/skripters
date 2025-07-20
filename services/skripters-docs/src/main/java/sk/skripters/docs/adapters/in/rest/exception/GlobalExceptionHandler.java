08143fa31afd63a8bfc26fcfc36a5d88950d7791
Jiří Veselý <vesely.jiri1998@gmail.com>
2025-07-20 20:55:10 +0200

    feat: add GlobalExceptionHandler

---
+++ services/skripters-docs/src/main/java/sk/skripters/docs/adapters/in/rest/exception/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package sk.skripters.docs.adapters.in.rest.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+}
